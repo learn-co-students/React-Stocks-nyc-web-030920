bc3e8983ee36310e47206f3962ee604b48954a9f
Samanthaponce5 <samanthaponce5@gmail.com>
2020-05-17 23:47:19 -0400

    Done.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,29 +1,32 @@
 import React from 'react';
 
-const SearchBar = () => {
+const SearchBar = (props) => {
   return (
+
+
+    
     <div>
 
       <strong>Sort by:</strong>
       <label>
-        <input type="radio" value="Alphabetically" checked={null} onChange={null}/>
-        Alphabetically
+        <input type="radio" name='sort' value='Alphabetically' checked={props.sort==='Alphabetically'} onChange={props.handleChange}/>
+        Alphabetically {props.sort==='Alphabetically'?'yes':'no'}
       </label>
       <label>
-        <input type="radio" value="Price" checked={null} onChange={null}/>
-        Price
+        <input type="radio" name='sort' value="Price" checked={props.sort==='Price'} onChange={props.handleChange}/>
+        Price {props.sort==='Price' ? 'yes':'no'}
       </label>
       <br/>
 
       <label>
         <strong>Filter:</strong>
-        <select onChange={null}>
+        <select name='filter' value={props.filter}  onChange={props.handleChange}>
           <option value="Tech">Tech</option>
           <option value="Sportswear">Sportswear</option>
           <option value="Finance">Finance</option>
         </select>
       </label>
-
+      sort:{props.filter}
 
     </div>
   );
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -4,16 +4,52 @@ import PortfolioContainer from './PortfolioContainer'
 import SearchBar from '../components/SearchBar'
 
 class MainContainer extends Component {
+  state= {
+    stocks:[],
+    sort:'',
+    filter:'Tech'
+    
+
+  }
+  
+  componentDidMount(){
+    fetch('http://localhost:3000/stocks')
+    .then((resp)=>resp.json())
+    .then((data)=>{
+      const {stocks}=this.state
+      this.setState({  stocks:data})
+    })
+  }
+
+  handleChange=(event)=>{
+    const{name, value, type, checked}= event.target
+    // type==='radio'? this.setState({[name]:checked}):this.setState({[name]:value})
+    this.setState({[name]:value})
+  }
 
   render() {
+ let allStocks=[...this.state.stocks]
+ if(this.state.sort==='Alphabetically'){
+  allStocks=this.state.stocks.sort((a,b)=>a.name.localeCompare(b.name))
+ }else if (this.state.sort==='Price'){
+   allStocks = this.state.stocks.sort((a,b)=>(a.price-b.price))
+ }else if(this.state.filter){
+   allStocks= this.state.stocks.filter((stock)=>{
+     if(stock.type === this.state.filter){
+       return stock
+     }
+   })
+ }
+ console.log(allStocks)
     return (
+     
       <div>
-        <SearchBar/>
+        <SearchBar  {...this.state} handleChange={this.handleChange}/>
 
           <div className="row">
             <div className="col-8">
 
-              <StockContainer/>
+              <StockContainer allStocks={allStocks}/>
 
             </div>
             <div className="col-4">
