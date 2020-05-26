9bf08e7d7b685c3aa2b40270cf800465af8f9207
komald4 <komald.4@gmail.com>
2020-05-26 16:37:39 -0400

    Done.

--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -7,9 +7,7 @@ class PortfolioContainer extends Component {
     return (
       <div>
         <h2>My Portfolio</h2>
-          {
-            //render your portfolio stocks here
-          }
+          {this.props.stocks.map(stock => <div onClick={() => this.props.removeStock(stock)}><Stock stock={stock}/> </div>)}
       </div>
     );
   }
--- src/containers/StockContainer.js
+++ src/containers/StockContainer.js
@@ -2,16 +2,15 @@ import React, { Component } from 'react';
 import Stock from '../components/Stock'
 
 class StockContainer extends Component {
-
+  
   render() {
+    let stocks = this.props.stocks
     return (
       <div>
         <h2>Stocks</h2>
-        {
-          //render the list of stocks here
-        }
+        {stocks.map(stock => <div onClick={() => this.props.addPortfolio(stock)}> <Stock  stock={stock} /></div> )}
       </div>
-    );
+    )
   }
 
 }
